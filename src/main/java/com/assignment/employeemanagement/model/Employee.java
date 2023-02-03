00e1eb929007fda4c0030b34da3618c948c5a7fc
Eynav Ptia <onevptaya@gmail.com>
2023-02-03 15:46:29 +0200

    Create employee model

---
+++ src/main/java/com/assignment/employeemanagement/model/Employee.java
@@ -0,0 +1,35 @@
+package com.assignment.employeemanagement.model;
+
+import jakarta.persistence.*;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    private static final AtomicInteger count = new AtomicInteger(0);
+    @Id
+    private int id;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "last_name")
+    private String last_name;
+    @Column(name = "email")
+    private String Email;
+
+    public Employee(String first_name, String last_name, String email) {
+        this.id = count.incrementAndGet();
+        this.first_name = first_name;
+        this.last_name = last_name;
+        Email = email;
+    }
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    public String getFirst_name() {return first_name;}
+    public void setFirst_name(String first_name) {this.first_name = first_name;}
+    public String getLast_name() {return last_name;}
+    public void setLast_name(String last_name) {this.last_name = last_name;}
+    public String getEmail() {return Email;}
+    public void setEmail(String email) {Email = email;}
+}
